7be3442216082215d2a2e84998d6384e0aec2be0
ruiserigrafia <ruiserigrafia@gmail.com>
2021-09-15 16:22:40 -0300

    Clone NETFLIX

---
+++ js/setup.js
@@ -0,0 +1,16 @@
+$('.carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,    
+    groupCells: false,
+    autoPlay: 3500,
+    pauseAutoPlayOnHover: false, 
+    setGallerySize: true,
+    contain: true,
+    pageDots: true,    
+    prevNextButtons: false,
+    freeScroll: true,
+    wrapAround: true 
+  });
+
+
